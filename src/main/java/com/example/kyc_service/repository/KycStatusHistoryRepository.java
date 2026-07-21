66f9f31d463103170d5efe8d85b4123196d98312
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-07-21 04:16:55 -0300

    feat: add method to retrieve KycStatusHistory by submission ID ordered by change date

--- src/main/java/com/example/kyc_service/repository/KycStatusHistoryRepository.java
+++ src/main/java/com/example/kyc_service/repository/KycStatusHistoryRepository.java
@@ -3,7 +3,9 @@ package com.example.kyc_service.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.kyc_service.model.KycStatusHistory;
+import java.util.List;
+import java.util.UUID;
 
-public interface KycStatusHistoryRepository extends JpaRepository<KycStatusHistory,Long>{
-    
+public interface KycStatusHistoryRepository extends JpaRepository<KycStatusHistory, Long> {
+    List<KycStatusHistory> findBySubmissionIdOrderByChangedAtAsc(UUID submissionId);
 }
